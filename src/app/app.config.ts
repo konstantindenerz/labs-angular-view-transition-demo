1ca8297454b152bb8b1505a6aa2991cdf3ebb321
Konstantin Denerz <konstantin.denerz@gmail.com>
2023-09-12 12:40:45 +0200

    update to project to support angular#73e4bf2ed2471faf44a49b591e19a390d5867449

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 import {ApplicationConfig} from '@angular/core';
 import {provideAnimations} from '@angular/platform-browser/animations';
-import {provideRouter, withComponentInputBinding, withHashLocation} from '@angular/router';
+import {provideRouter, withComponentInputBinding, withHashLocation, withViewTransitions } from '@angular/router';
 
 import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withHashLocation(), withComponentInputBinding()), provideAnimations()],
+  providers: [provideRouter(routes, withHashLocation(), withComponentInputBinding(), withViewTransitions()), provideAnimations()],
 };
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,5 @@ import {bootstrapApplication} from '@angular/platform-browser';
 import {AppComponent} from './app/app.component';
 import {appConfig} from './app/app.config';
 
-// Workaround to avoid this issue: https://bugs.chromium.org/p/chromium/issues/detail?id=1434757
-setTimeout(() => {
-  bootstrapApplication(AppComponent, appConfig)
-    .catch((err) => console.error(err));
-}, 10);
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
